cbd9c51d287d30e5fb75545d523615363b74407d
Tracks12 <cardinal.florian.erwan@gmail.com>
2025-07-07 15:50:49 +0200

    feat(wifi): add feature to get wifi password to registered network on windows netsh manager

--- tools/wifi.py
+++ tools/wifi.py
@@ -3,6 +3,8 @@
 
 # tools/wifi.py
 
+from subprocess import check_output as prompt
+
 from core.tool import Tool
 
 class Wifi(Tool):
@@ -16,16 +18,32 @@ class Wifi(Tool):
 
 	def __init__(self, args: list[str]):
 		self._args	= [
-			(("-s", "--say-hello", ""), "Say a hello world")
+			(("-p", "--password", ""), "Get the password of the registered wifi network"),
 		]
 
 		self._execs = [
-			lambda x:self._sayHello(x)
+			lambda x:self._getWifiPasswords()
 		]
 
 		super().__init__()
 		self._run(args)
 
-	def _sayHello(self, args: list[str]) -> bool:
-		print(f"Hello world :D")
-		return(True)
+	def _getWifiPasswords(self) -> None:
+		__cmd	= prompt(["netsh", "wlan", "show", "profiles"]).decode("ansi").split("\n")
+		__wifis	= [ line.split(":")[1][1:-1] for line in __cmd if("Profil Tous les utilisateurs" in line) ]
+
+		_m		= 1
+		_s		= 20
+		__table	= [ f"{' '*_m}*  {'Name':<{_s}}Password", "" ]
+
+		for i, wifi in enumerate(__wifis, 1):
+			__cmd = prompt(["netsh", "wlan", "show", "profiles", wifi, "key=clear"]).decode("ansi").split("\n")
+			__password = [ line.split(":")[1][1:-1] for line in __cmd if("Contenu de la cl" in line) ]
+
+			try:
+				__table.append(f"{' '*_m}{i}. {wifi:<{_s}}{__password[0]}")
+
+			except Exception:
+				__table.append(f"{' '*_m}{i}. {wifi:<{_s}}-")
+
+		print("\n".join(__table))
